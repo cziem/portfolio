92a876b3d9fdaa88784d6b6d45ef71fda6b539fc
Fay Vor <phavorsparks@gmail.com>
2018-07-29 02:10:59 +0100

    Basic component setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link, Navlink, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/style.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is coming from dashboard component</div>
+);
+
+const AddExpensePage = () => <div>Add some expenses here</div>;
+
+const EditPageExpensePage = () => <div>This is the edit page!</div>;
+
+const helpPage = () => <div>This is the help page</div>;
+
+const NotFound = () => (
+  <div>
+    404 page not found! <Link to="/">Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
+
+const Routes = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditPageExpensePage} />
+        <Route path="/help" component={helpPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<Routes />, document.getElementById("app"));
